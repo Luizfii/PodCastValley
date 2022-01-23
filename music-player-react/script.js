71c15c1a0c1367e38d999ca0bc599e4e203dd1bb
Luiz Filipe dos Santos Paula <74314665+Luizfii@users.noreply.github.com>
2022-01-22 22:53:41 -0300

    primeira versao criada em 22 de janeiro
    criaçao frontend.
    criaçao de splash screen;
    criaçao da primeira parte da pagina principal;
    criaçao da primeira parte do player;

---
+++ music-player-react/script.js
@@ -0,0 +1,5 @@
+function mudaTela(){
+    document.getElementsByClassName("logo-splash")[0].style.display= "none";
+    document.getElementsByClassName("logo-header")[0].style.display= "block";
+    
+}
