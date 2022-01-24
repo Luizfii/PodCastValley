9dd620ee13485ba180f224275f7505f7f210d068
Luiz Filipe dos Santos Paula <74314665+Luizfii@users.noreply.github.com>
2022-01-24 05:17:52 -0300

    segunda versão
    player css quase configurado. adição de botões e funções para os botoes.

--- music-player-react/script.js
+++ music-player-react/script.js
@@ -3,3 +3,29 @@ function mudaTela(){
     document.getElementsByClassName("logo-header")[0].style.display= "block";
     
 }
+
+function repete(){
+    console.log ("repetir faixa");
+}
+
+function avanca(){
+    console.log ("avançou");
+}
+
+function inicia(){
+    document.getElementsByClassName("botaoPlay-container")[0].style.display= "none";
+    document.getElementsByClassName("botaoPause-container")[0].style.display= "flex";
+}
+
+function pausa(){
+    document.getElementsByClassName("botaoPlay-container")[0].style.display= "flex";
+    document.getElementsByClassName("botaoPause-container")[0].style.display= "none";
+}
+
+function retrocede(){
+    console.log ("retrocedeu");
+}
+
+function tocaAleatorio(){
+    console.log ("toca faixas aleatoriamente")
+}
